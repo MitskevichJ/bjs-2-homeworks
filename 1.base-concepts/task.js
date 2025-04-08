f7ed769f43b894db57d50dc75832f8a2a3925c80
Mitskevich <smirnovaj010@gmail.com>
2025-04-08 17:52:49 +0300

    Изменения первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,30 +1,24 @@
-Задача №1
 "use strict";
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  const d = b**2 - 4 * a * c; 
+	let arr = [];
+	let d = Math.pow(b, 2) - 4 * a * c;
 
-  if (d < 0) {
-  } else if (d === 0) {
-    const root = -b / (2 * a);
-    arr.push(root);
-  } else {
-    const root1 = (-b + Math.sqrt(d)) / (2 * a);
-    const root2 = (-b - Math.sqrt(d)) / (2 * a);
-    arr.push(root1, root2);
-  }
-
-  return arr;
+	if (d < 0) {
+		return arr;
+	} else if (d === 0) {
+		let root = -b / (2 * a);
+		arr.push(root);
+		return arr;
+	} else {
+		let root1 = (-b + Math.sqrt(d)) / (2 * a);
+		let root2 = (-b - Math.sqrt(d)) / (2 * a);
+		arr.push(root1, root2);
+		return arr;
+	}
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-}
-const result1 = solveEquation(1, -5, 6); 
-console.log(result1); 
-
-const result2 = solveEquation(1, 2, 1); 
-console.log(result2); 
-
-const result3 = solveEquation(1, 1, 1); 
-console.log(result3); 
+console.log(solveEquation(1, -3, 2)); 
+console.log(solveEquation(1, 2, 1)); 
+console.log(solveEquation(1, 0, -4)); 
+console.log(solveEquation(1, 0, 4)); 
