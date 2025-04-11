e0f1dd47f9f144ef899811ce88af4a9d7fbee84b
Mitskevich <smirnovaj010@gmail.com>
2025-04-11 11:57:15 +0300

    Финал изменений Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,21 +4,19 @@ function solveEquation(a, b, c) {
 	let arr = [];
 	let d = Math.pow(b, 2) - 4 * a * c;
 
-	if (d < 0) {
-		return arr;
-	} else if (d === 0) {
+	if (d < 0) {} else if (d === 0) {
 		let root = -b / (2 * a);
 		arr.push(root);
-		return arr;
 	} else {
 		let root1 = (-b + Math.sqrt(d)) / (2 * a);
 		let root2 = (-b - Math.sqrt(d)) / (2 * a);
 		arr.push(root1, root2);
-		return arr;
 	}
+
+	return arr;
 }
 
-console.log(solveEquation(1, -3, 2)); 
-console.log(solveEquation(1, 2, 1)); 
-console.log(solveEquation(1, 0, -4)); 
-console.log(solveEquation(1, 0, 4)); 
+console.log(solveEquation(1, -3, 2));
+console.log(solveEquation(1, 2, 1));
+console.log(solveEquation(1, 0, -4));
+console.log(solveEquation(1, 0, 4));
