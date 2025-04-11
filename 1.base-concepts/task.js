fd5e421eea9559b8cd0fa0208fa18290a9da18e4
Mitskevich <smirnovaj010@gmail.com>
2025-04-11 12:16:24 +0300

    Первое и второе задания изменения

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,19 +1,19 @@
 "use strict";
 
 function solveEquation(a, b, c) {
-	let arr = [];
-	let d = Math.pow(b, 2) - 4 * a * c;
+	const discriminant = b * b - 4 * a * c;
+	let roots = [];
 
-	if (d < 0) {} else if (d === 0) {
-		let root = -b / (2 * a);
-		arr.push(root);
-	} else {
-		let root1 = (-b + Math.sqrt(d)) / (2 * a);
-		let root2 = (-b - Math.sqrt(d)) / (2 * a);
-		arr.push(root1, root2);
+	if (discriminant === 0) {
+		const root = -b / (2 * a);
+		roots = [root];
+	} else if (discriminant > 0) {
+		const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		roots = [root1, root2];
 	}
 
-	return arr;
+	return roots;
 }
 
 console.log(solveEquation(1, -3, 2));
@@ -21,12 +21,29 @@ console.log(solveEquation(1, 2, 1));
 console.log(solveEquation(1, 0, -4));
 console.log(solveEquation(1, 0, 4));
 
-Задание 2
+function solveEquation(a, b, c) {
+	const discriminant = b * b - 4 * a * c;
+
+	if (discriminant < 0) {
+		return [];
+	} else if (discriminant === 0) {
+		const root = -b / (2 * a);
+		return [root];
+	} else {
+		const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		return [root1, root2];
+	}
+}
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 	const monthlyPercent = (percent / 100) / 12;
 
 	const loanBody = amount - contribution;
+
+	if (loanBody <= 0) {
+		return 0;
+	}
 
 	const monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / ((Math.pow(1 + monthlyPercent, countMonths)) - 1)));
 
