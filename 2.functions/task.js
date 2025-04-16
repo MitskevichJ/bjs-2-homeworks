df045759f4c5c28240651a77dd482849d68cfdfc
Mitskevich <smirnovaj010@gmail.com>
2025-04-16 17:43:37 +0300

    Первое задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,27 @@
 function getArrayParams(...arr) {
-    let min = Infinity;
-    let max = -Infinity;
-    let sum = 0;
+	if (arr.length === 0) {
+		return {};
+	}
 
-    if (arr.length === 0) {
-        return { min: 0, max: 0, avg: 0 };
-    }
+	let min = Infinity;
+	let max = -Infinity;
+	let sum = 0;
 
-    for (let num of arr) {
-        if (num < min) {
-            min = num;
-        }
-        if (num > max) {
-            max = num;
-        }
-        sum += num;
-    }
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		sum += arr[i];
+	}
 
-    const avg = (sum / arr.length).toFixed(2);
+	const avg = Number((sum / arr.length).toFixed(2));
 
-    return {
-        min: min,
-        max: max,
-        avg: Number(avg)
-    };
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
-
-console.log(getArrayParams(-99, 99, 10));
-console.log(getArrayParams(1, 2, 3, -100, 10));
-console.log(getArrayParams(5));
